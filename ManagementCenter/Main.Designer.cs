d81c0c753a905f3cc43a7b1cc0157a3a58564bd3
John <katway@163.com>
2013-12-05 14:09:20 +0800

    添加主窗体中的左侧导航菜单

--- ManagementCenter/Main.Designer.cs
+++ ManagementCenter/Main.Designer.cs
@@ -28,19 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.foldingMenuStrip1 = new OpenSource.PhhSoft.FoldingMenuStrip();
+            this.foldingMenuItem1 = new OpenSource.PhhSoft.Support.FoldingMenuItem();
+            this.foldingMenuItem2 = new OpenSource.PhhSoft.Support.FoldingMenuItem();
+            this.foldingMenuItem3 = new OpenSource.PhhSoft.Support.FoldingMenuItem();
+            this.foldingButtonItem1 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem2 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem3 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem4 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem5 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem6 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem8 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem9 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingButtonItem10 = new OpenSource.PhhSoft.Support.FoldingButtonItem();
+            this.foldingMenuStrip1.SuspendLayout();
+            this.foldingMenuItem1.InnerPanel.SuspendLayout();
+            this.foldingMenuItem2.InnerPanel.SuspendLayout();
+            this.foldingMenuItem3.InnerPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // foldingMenuStrip1
+            // 
+            this.foldingMenuStrip1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.foldingMenuStrip1.AItems.AddRange(new OpenSource.PhhSoft.Support.FoldingMenuItem[] {
+            this.foldingMenuItem1,
+            this.foldingMenuItem2,
+            this.foldingMenuItem3});
+            this.foldingMenuStrip1.Controls.Add(this.foldingMenuItem1);
+            this.foldingMenuStrip1.Controls.Add(this.foldingMenuItem2);
+            this.foldingMenuStrip1.Controls.Add(this.foldingMenuItem3);
+            this.foldingMenuStrip1.CurrentItem = this.foldingMenuItem2;
+            this.foldingMenuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.foldingMenuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.foldingMenuStrip1.Name = "foldingMenuStrip1";
+            this.foldingMenuStrip1.Size = new System.Drawing.Size(183, 481);
+            this.foldingMenuStrip1.TabIndex = 0;
+            this.foldingMenuStrip1.Text = "foldingMenuStrip1";
+            // 
+            // foldingMenuItem1
+            // 
+            this.foldingMenuItem1.ButtonItems.AddRange(new OpenSource.PhhSoft.Support.FoldingButtonItem[] {
+            this.foldingButtonItem1,
+            this.foldingButtonItem2,
+            this.foldingButtonItem3,
+            this.foldingButtonItem4});
+            // 
+            // 
+            // 
+            this.foldingMenuItem1.InnerPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.foldingMenuItem1.InnerPanel.Controls.Add(this.foldingButtonItem1);
+            this.foldingMenuItem1.InnerPanel.Controls.Add(this.foldingButtonItem2);
+            this.foldingMenuItem1.InnerPanel.Controls.Add(this.foldingButtonItem3);
+            this.foldingMenuItem1.InnerPanel.Controls.Add(this.foldingButtonItem4);
+            this.foldingMenuItem1.InnerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.foldingMenuItem1.InnerPanel.Location = new System.Drawing.Point(1, 30);
+            this.foldingMenuItem1.InnerPanel.Name = "panel1";
+            this.foldingMenuItem1.InnerPanel.Size = new System.Drawing.Size(181, 0);
+            this.foldingMenuItem1.InnerPanel.TabIndex = 0;
+            this.foldingMenuItem1.IsOpened = false;
+            this.foldingMenuItem1.Location = new System.Drawing.Point(0, 0);
+            this.foldingMenuItem1.Margin = new System.Windows.Forms.Padding(1);
+            this.foldingMenuItem1.Name = "foldingMenuItem1";
+            this.foldingMenuItem1.Padding = new System.Windows.Forms.Padding(1, 30, 1, 0);
+            this.foldingMenuItem1.Size = new System.Drawing.Size(183, 30);
+            this.foldingMenuItem1.TabIndex = 0;
+            this.foldingMenuItem1.TabStop = false;
+            this.foldingMenuItem1.Text = "FoldingMenuItem";
+            // 
+            // foldingMenuItem2
+            // 
+            this.foldingMenuItem2.ButtonItems.AddRange(new OpenSource.PhhSoft.Support.FoldingButtonItem[] {
+            this.foldingButtonItem5,
+            this.foldingButtonItem6});
+            // 
+            // 
+            // 
+            this.foldingMenuItem2.InnerPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.foldingMenuItem2.InnerPanel.Controls.Add(this.foldingButtonItem5);
+            this.foldingMenuItem2.InnerPanel.Controls.Add(this.foldingButtonItem6);
+            this.foldingMenuItem2.InnerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.foldingMenuItem2.InnerPanel.Location = new System.Drawing.Point(1, 30);
+            this.foldingMenuItem2.InnerPanel.Name = "panel1";
+            this.foldingMenuItem2.InnerPanel.Size = new System.Drawing.Size(181, 52);
+            this.foldingMenuItem2.InnerPanel.TabIndex = 0;
+            this.foldingMenuItem2.IsOpened = true;
+            this.foldingMenuItem2.Location = new System.Drawing.Point(0, 29);
+            this.foldingMenuItem2.Margin = new System.Windows.Forms.Padding(1);
+            this.foldingMenuItem2.Name = "foldingMenuItem2";
+            this.foldingMenuItem2.Padding = new System.Windows.Forms.Padding(1, 30, 1, 0);
+            this.foldingMenuItem2.Size = new System.Drawing.Size(183, 82);
+            this.foldingMenuItem2.TabIndex = 1;
+            this.foldingMenuItem2.TabStop = false;
+            this.foldingMenuItem2.Text = "FoldingMenuItem";
+            // 
+            // foldingMenuItem3
+            // 
+            this.foldingMenuItem3.ButtonItems.AddRange(new OpenSource.PhhSoft.Support.FoldingButtonItem[] {
+            this.foldingButtonItem8,
+            this.foldingButtonItem9,
+            this.foldingButtonItem10});
+            // 
+            // 
+            // 
+            this.foldingMenuItem3.InnerPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.foldingMenuItem3.InnerPanel.Controls.Add(this.foldingButtonItem8);
+            this.foldingMenuItem3.InnerPanel.Controls.Add(this.foldingButtonItem9);
+            this.foldingMenuItem3.InnerPanel.Controls.Add(this.foldingButtonItem10);
+            this.foldingMenuItem3.InnerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.foldingMenuItem3.InnerPanel.Location = new System.Drawing.Point(1, 30);
+            this.foldingMenuItem3.InnerPanel.Name = "panel1";
+            this.foldingMenuItem3.InnerPanel.Size = new System.Drawing.Size(181, 0);
+            this.foldingMenuItem3.InnerPanel.TabIndex = 0;
+            this.foldingMenuItem3.IsOpened = false;
+            this.foldingMenuItem3.Location = new System.Drawing.Point(0, 451);
+            this.foldingMenuItem3.Margin = new System.Windows.Forms.Padding(1);
+            this.foldingMenuItem3.Name = "foldingMenuItem3";
+            this.foldingMenuItem3.Padding = new System.Windows.Forms.Padding(1, 30, 1, 0);
+            this.foldingMenuItem3.Size = new System.Drawing.Size(183, 30);
+            this.foldingMenuItem3.TabIndex = 2;
+            this.foldingMenuItem3.TabStop = false;
+            this.foldingMenuItem3.Text = "FoldingMenuItem";
+            // 
+            // foldingButtonItem1
+            // 
+            this.foldingButtonItem1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.foldingButtonItem1.Location = new System.Drawing.Point(0, 0);
+            this.foldingButtonItem1.Name = "foldingButtonItem1";
+            this.foldingButtonItem1.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem1.TabIndex = 0;
+            this.foldingButtonItem1.Text = "DropDownButton";
+            // 
+            // foldingButtonItem2
+            // 
+            this.foldingButtonItem2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.foldingButtonItem2.Location = new System.Drawing.Point(0, -78);
+            this.foldingButtonItem2.Name = "foldingButtonItem2";
+            this.foldingButtonItem2.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem2.TabIndex = 1;
+            this.foldingButtonItem2.Text = "DropDownButton";
+            // 
+            // foldingButtonItem3
+            // 
+            this.foldingButtonItem3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.foldingButtonItem3.Location = new System.Drawing.Point(0, -52);
+            this.foldingButtonItem3.Name = "foldingButtonItem3";
+            this.foldingButtonItem3.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem3.TabIndex = 2;
+            this.foldingButtonItem3.Text = "DropDownButton";
+            // 
+            // foldingButtonItem4
+            // 
+            this.foldingButtonItem4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.foldingButtonItem4.Location = new System.Drawing.Point(0, -26);
+            this.foldingButtonItem4.Name = "foldingButtonItem4";
+            this.foldingButtonItem4.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem4.TabIndex = 3;
+            this.foldingButtonItem4.Text = "DropDownButton";
+            // 
+            // foldingButtonItem5
+            // 
+            this.foldingButtonItem5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.foldingButtonItem5.Location = new System.Drawing.Point(0, 0);
+            this.foldingButtonItem5.Name = "foldingButtonItem5";
+            this.foldingButtonItem5.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem5.TabIndex = 0;
+            this.foldingButtonItem5.Text = "DropDownButton";
+            // 
+            // foldingButtonItem6
+            // 
+            this.foldingButtonItem6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.foldingButtonItem6.Location = new System.Drawing.Point(0, 26);
+            this.foldingButtonItem6.Name = "foldingButtonItem6";
+            this.foldingButtonItem6.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem6.TabIndex = 1;
+            this.foldingButtonItem6.Text = "DropDownButton";
+            // 
+            // foldingButtonItem8
+            // 
+            this.foldingButtonItem8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.foldingButtonItem8.Location = new System.Drawing.Point(0, 0);
+            this.foldingButtonItem8.Name = "foldingButtonItem8";
+            this.foldingButtonItem8.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem8.TabIndex = 0;
+            this.foldingButtonItem8.Text = "DropDownButton";
+            // 
+            // foldingButtonItem9
+            // 
+            this.foldingButtonItem9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.foldingButtonItem9.Location = new System.Drawing.Point(0, -52);
+            this.foldingButtonItem9.Name = "foldingButtonItem9";
+            this.foldingButtonItem9.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem9.TabIndex = 1;
+            this.foldingButtonItem9.Text = "DropDownButton";
+            // 
+            // foldingButtonItem10
+            // 
+            this.foldingButtonItem10.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.foldingButtonItem10.Location = new System.Drawing.Point(0, -26);
+            this.foldingButtonItem10.Name = "foldingButtonItem10";
+            this.foldingButtonItem10.Size = new System.Drawing.Size(181, 26);
+            this.foldingButtonItem10.TabIndex = 2;
+            this.foldingButtonItem10.Text = "DropDownButton";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(706, 357);
+            this.ClientSize = new System.Drawing.Size(837, 481);
+            this.Controls.Add(this.foldingMenuStrip1);
             this.Name = "Main";
             this.Text = "Main";
+            this.foldingMenuStrip1.ResumeLayout(false);
+            this.foldingMenuItem1.InnerPanel.ResumeLayout(false);
+            this.foldingMenuItem2.InnerPanel.ResumeLayout(false);
+            this.foldingMenuItem3.InnerPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private OpenSource.PhhSoft.FoldingMenuStrip foldingMenuStrip1;
+        private OpenSource.PhhSoft.Support.FoldingMenuItem foldingMenuItem1;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem1;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem2;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem3;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem4;
+        private OpenSource.PhhSoft.Support.FoldingMenuItem foldingMenuItem2;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem5;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem6;
+        private OpenSource.PhhSoft.Support.FoldingMenuItem foldingMenuItem3;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem8;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem9;
+        private OpenSource.PhhSoft.Support.FoldingButtonItem foldingButtonItem10;
     }
 }
